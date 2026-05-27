8b45de7923704afe250065c322f971be0b9a1c57
rey <reyyanpak0@gmail.com>
2026-05-28 01:39:35 +0200

    exported assignment-1a executable

--- running-project/node_2d.gd
+++ running-project/node_2d.gd
@@ -2,7 +2,11 @@ extends Node2D
 
 func _process(delta):
 	# Continuously rotate all child nodes 
-	# delta ensures the speed is the same regardless of framerate [cite: 104]
 	for child in get_children():
 		if child is Area2D:
-			child.rotation += 2.0 * delta
+			child.rotation += 2.0 * delta # delta ensures the speed is the same regardless of framerate
+
+var message = "Hello Godot"
+
+func _ready():
+	print(message)
