af29da54f5fc198d0841926cc6ee37324010881b
rey <reyyanpak0@gmail.com>
2026-05-28 03:10:59 +0200

    exported assignment-2a executable

--- running-project/node_2d.gd
+++ running-project/node_2d.gd
@@ -6,7 +6,10 @@ func _process(delta):
 		if child is Area2D:
 			child.rotation += 2.0 * delta # delta ensures the speed is the same regardless of framerate
 
-var message = "Hello Godot"
-
-func _ready():
-	print(message)
+func _unhandled_input(event):
+	if event.is_action_pressed("color_changed_1"):
+		$Area2D_1.modulate = Color.RED
+	elif event.is_action_pressed("color_changed_2"):
+		$Area2D_2.modulate = Color.GREEN
+	elif event.is_action_pressed("color_changed_3"):
+		$Area2D_3.modulate = Color.BLUE
