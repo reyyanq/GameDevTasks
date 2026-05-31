cb3ee8db7767f9372d065c9b3449b2c6ea699422
rey <reyyanpak0@gmail.com>
2026-05-31 04:43:08 +0200

    completed 2D Graphics and Editor Basics

--- running-project/area_2d_3.gd
+++
@@ -1,5 +0,0 @@
-extends Area2D
-
-func _on_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		modulate = Color.BLUE 
--- running-project/node_2d.gd
+++ running-project/node_2d.gd
@@ -1,15 +1,4 @@
 extends Node2D
 
 func _process(delta):
-	# Continuously rotate all child nodes 
-	for child in get_children():
-		if child is Area2D:
-			child.rotation += 2.0 * delta # delta ensures the speed is the same regardless of framerate
-
-func _unhandled_input(event):
-	if event.is_action_pressed("color_changed_1"):
-		$Area2D_1.modulate = Color.RED
-	elif event.is_action_pressed("color_changed_2"):
-		$Area2D_2.modulate = Color.GREEN
-	elif event.is_action_pressed("color_changed_3"):
-		$Area2D_3.modulate = Color.BLUE
+	pass
